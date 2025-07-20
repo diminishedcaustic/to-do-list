f9ed77f0741e4b4d80d831afe500aeadca6c8f88
diminishedcaustic <157384379+diminishedcaustic@users.noreply.github.com>
2025-07-20 14:20:24 +0800

    added delete button

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ const addButton = document.querySelector("#add");
 const inputTextField = document.querySelector("#input-text-field")
 
 addButton.addEventListener('click', () => {
-    if(inputTextField.value !== ''){
-        let newItem = document.createElement("li")
-        newItem.textContent = inputTextField.value
+    if(inputTextField.value != ''){
+        let newItem = document.createElement("li");
+        newItem.textContent = inputTextField.value;
+        
+        let newButton = document.createElement("button");
+        newButton.textContent = 'x';
+        newButton.classList.add("remove-button");
 
-        toDoList.append(newItem)
-        inputTextField.value = ""
+        newButton.addEventListener('click', () => {
+            let parent = newButton.parentElement
+            parent.parentElement.removeChild(parent);
+        })
+
+        newItem.append(newButton);
+        
+        toDoList.append(newItem);
+        inputTextField.value = '';
     }
-})
+});
